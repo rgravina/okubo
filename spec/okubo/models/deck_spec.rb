771513daebbc795e96c21f84749e9567a2de3a59
Robert Gravina <robert@gravina.com>
2012-10-26 23:28:30 +0900

    Removed some duplicate specs

--- spec/okubo/models/deck_spec.rb
+++ spec/okubo/models/deck_spec.rb
@@ -16,11 +16,6 @@ describe Okubo::Deck do
     it "should remove words from the word list (but not the model itself)"
   end
 
-  context "Browsing word lists" do
-    it "should allow user to iterate through all words"
-    it "should allow user to iterate words scheduled for review only"
-  end
-
   context "Cleaning up" do
     it "should delete itself and all item information when the source model is deleted" do
       deck = @user.words
