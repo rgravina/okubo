dcdf73795b4444fa450cb4453d2dc6af64437db6
Robert Gravina <robert@gravina.com>
2013-09-10 17:43:32 +0900

    Fix for find_or_create depreciation warnings.

--- lib/okubo/deck_methods.rb
+++ lib/okubo/deck_methods.rb
@@ -1,7 +1,7 @@
 module Okubo
   module DeckMethods
     def deck
-      d = Okubo::Deck.find_or_create_by_user_id_and_user_type(self.id, self.class.name)
+      d = Okubo::Deck.where(:user_id => self.id, :user_type => self.class.name).first_or_create
       d.source_class.module_eval do 
         def stats
          Okubo::Item.first(:conditions => {:source_id => self.id, :source_type => self.class.name})
